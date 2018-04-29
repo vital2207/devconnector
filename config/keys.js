dd55d0e285588a629638fba61330e520d8520bc6
Vitaly <vital2207@yandex.ru>
2018-04-29 23:46:24 +0300

    Added api for JWT autorization users and validation for it

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://vital2207:1234@ds011412.mlab.com:11412/devconnector"
+  mongoURI: "mongodb://vital2207:1234@ds011412.mlab.com:11412/devconnector",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -7,6 +9,10 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //DB config
 
 const db = require("./config/keys").mongoURI;
@@ -18,7 +24,12 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+
+require("./config/passport")(passport);
 
 //Use routes
 
