0685693676a492a03004963cd3b6d856449bb903
Vitaly <vital2207@yandex.ru>
2018-05-01 16:46:37 +0300

    implemented redux boilerplate

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+const inititalState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = inititalState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
